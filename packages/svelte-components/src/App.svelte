58368dde4de0c6cef534373710a6a47647ba2026
leo9iota <leo9iota@gmail.com>
2025-08-23 20:00:37 +0200

    chore(svelte): remove unused files

--- packages/svelte-components/src/App.svelte
+++
@@ -1,7 +0,0 @@
-<div>
-  <h1
-    style="background: red; color: white; padding: 40px; font-size: 48px; position: fixed; top: 0; left: 0; width: 100%; z-index: 9999;"
-  >
-    HELLO WORLD - SVELTE IS WORKING
-  </h1>
-</div>
--- packages/svelte-components/src/main.ts
+++
@@ -1,8 +0,0 @@
-// import './app.css'  // Temporarily commented out
-import App from './App.svelte';
-
-const app = new App({
-    target: document.getElementById('app')!,
-});
-
-export default app;
