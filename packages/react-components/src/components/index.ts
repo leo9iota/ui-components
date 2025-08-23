74065242f6c38e94b352bc6d7fa944e498a01814
leo9iota <leo9iota@gmail.com>
2025-08-23 18:45:12 +0200

    refactor(index): simplify export statement

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -1,2 +1,1 @@
-export { default } from './SocialLogin/SocialLogin';
-export type { SocialLoginProps } from './SocialLogin/SocialLogin';
+export { default, type SocialLoginProps } from './SocialLogin/SocialLogin';
